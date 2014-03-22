9d32fe98238b02208f2ec00cd2ffce33e7bcac44
Nam Chu Hoai <nambrot@googlemail.com>
2014-03-22 12:51:30 -0400

    Add Rails Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Memex::Application.config.secret_key_base = 'c55758185740ddac76d6c91dc4e0651572c3634104331dffeff7a42a00e5ce61846df5ae819876dd45cafd47eb2b1185735c0f5667af7709b7c45465d6696943'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Memex::Application.config.session_store :cookie_store, key: '_memex_session'
