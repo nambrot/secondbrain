5085b8dc648ab2492cf5ccbc0416f899512286dc
Nam Chu Hoai <nambrot@googlemail.com>
2014-03-22 17:07:02 -0400

    put Elastic search in env var

--- config/initializers/flex.rb
+++ config/initializers/flex.rb
@@ -1,4 +1,5 @@
 Flex::Configuration.configure do |config|
+   config.http_client.base_uri = 'http://localhost:9222' || ENV['ELASTIC_SEARCH_URL']
    config.flex_models = %w[ Url Product ]
    config.flex_active_models = %w[ Url Product ]
    config.variables[:index] = "url"
